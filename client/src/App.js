98f9410e8b9006eb131f8cf88985ee8d3fa49c26
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-05-26 21:38:07 -0300

    creacion de Profile + styles

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,18 @@ import Landing from "./Components/Landing/Landing";
 import Home from './Components/Home';
 import AddOwner from './Components/Forms/AddOwner'
 import AddPet from "./Components/Forms/AddPet";
-
+import Profile from "./Views/Profile/Profile.jsx"
 
 function App() {
   return (
     <BrowserRouter>      
       <div className="App">
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/agregarUsuario' element={<AddOwner />} />
-          <Route path='/agregarMascota' element={<AddPet />} />
-
-
+          <Route path="/" element={<Landing/>} />
+          <Route path='/home' element={<Home/>} />
+          <Route path='/agregarUsuario' element={<AddOwner/>} />
+          <Route path='/agregarMascota' element={<AddPet/>} />
+          <Route path="/user" element={<Profile/>}/>
         </Routes>
       </div>
     </BrowserRouter>
