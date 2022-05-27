1100a3bcf925a1d93fdafb08ca1494ec5e0abdcc
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-05-27 14:50:35 -0300

    nueva navbar segun vista + estilos

--- client/src/Components/NavBar/NavBarRegistered.jsx
+++ client/src/Components/NavBar/NavBarRegistered.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./NavBar.module.css";
+import styles from "./NavBarRegistered.module.css";
 import Button from "../GlobalCss/Button.module.css";
 import global from "../GlobalCss/Global.module.css";
 import OutContainer from "../GlobalCss/OutContainer.module.css";
@@ -13,23 +13,23 @@ function NavBar() {
     
     <div className={OutContainer.container}>
       <nav className={styles.nav}>
-        <span>LOGO</span>
+        <span>Hola, {user.name}</span>
 
         <section className={styles.contents}>
           <ul className={styles.navList}>
             <li className={styles.navItem}>
               <a href="#" className={styles.navLink}>
-                Linksdijdsihsdh
+                Acerca de nosotros
               </a>
             </li>
             <li className={styles.navItem}>
               <a href="#" className={styles.navLink}>
-                Link2
+                Petshop
               </a>
             </li>
             <li className={styles.navItem}>
               <a href="#" className={styles.navLink}>
-                Link3
+                Contacto
               </a>
             </li>
           </ul>
