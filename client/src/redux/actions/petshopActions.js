bb9e10181672a0fa421d7e381949fe4088841a55
Sabri-Rapa <sabrina.rapa@hotmail.com>
2022-05-30 02:12:06 -0300

    Filtros, ordenamiento y searchbar de petshop

--- client/src/redux/actions/petshopActions.js
+++ client/src/redux/actions/petshopActions.js
@@ -1,9 +1,19 @@
 import axios from "axios";
-import { GET_PRODUCTS, SEARCHBAR_PRODUCTS, FILTER_BY_PET } from "../actions-type/petshopActionsTypes";
+import { GET_PRODUCTS, SEARCHBAR_PRODUCTS, FILTER_BY_PET, SORT_PRICE, FILTER_CATEGORY, FILTER_TARGET_ANIMAL} from "../actions-type/petshopActionsTypes";
 
-export function getProducts (filter, order){
+// export function getProducts (filter, order){
+//     return async function (dispatch){
+//         let response = await axios.get(`http://localhost:3001/products?filter=${filter || ''}&order=${order || 'ASC'}`);
+//         return dispatch({
+//             type: GET_PRODUCTS,
+//             payload: response.data
+//         })
+//     }
+// }
+
+export function getProducts (){
     return async function (dispatch){
-        let response = await axios.get(`http://localhost:3001/products?filter=${filter || ''}&order=${order || 'ASC'}`);
+        let response = await axios.get('http://localhost:3001/products');
         return dispatch({
             type: GET_PRODUCTS,
             payload: response.data
@@ -25,6 +35,28 @@ export function searchBarProducts (name){
 export function filterByPet (payload){
     return {
         type: FILTER_BY_PET,
+        payload,
+    }
+}
+
+
+export function sortByPrice(payload){
+    return {
+        type: SORT_PRICE,
+        payload,
+    }
+}
+
+export function filterByCategory(payload){
+    return {
+        type: FILTER_CATEGORY,
+        payload,
+    }
+}
+
+export function filterTargetAnimal(payload){
+    return {
+        type: FILTER_TARGET_ANIMAL,
         payload,
     }
 }
