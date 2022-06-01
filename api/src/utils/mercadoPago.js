76a941533bf2c5a4a64d34a39e2f9635565dc054
Sabri-Rapa <sabrina.rapa@hotmail.com>
2022-06-01 13:14:09 -0300

    avance MP

---
+++ api/src/utils/mercadoPago.js
@@ -0,0 +1,10 @@
+const mercadopago = require('mercadopago')
+require('dotenv').config();
+
+mercadopago.configure({
+    access_token: process.env.ACCES_TOKEN,
+});
+
+module.exports = {
+    mercadopago
+}
