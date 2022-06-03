11abd7b864d8391a69cf9b344506ec4eefa25382
zm0x7b9 <zm0x7b9@gmail.com>
2022-06-03 09:22:54 -0300

    () Bug Carrito Total solucionado

--- client/src/Components/Shop/ProductCard.jsx
+++ client/src/Components/Shop/ProductCard.jsx
@@ -7,59 +7,59 @@ import { Link } from "react-router-dom";
 
 import axios from "axios";
 
-const ProductCard = ({profilePicture, name, price, isFavorite, id, setFavorites, favorites}) => {
-  const {user} = useAuth0();
+const ProductCard = ({ profilePicture, name, price, isFavorite, id, setFavorites, favorites }) => {
+  const { user } = useAuth0();
   const dispatch = useDispatch()
-  const addFavorite = async()=>{
-    if(!isFavorite){
+  const addFavorite = async () => {
+    if (!isFavorite) {
       const AllOwners = await axios.get("http://localhost:3001/owners")
       // console.log(owner)
-      const owner = AllOwners.data.find(x=>x.email === user.email)
+      const owner = AllOwners.data.find(x => x.email === user.email)
       console.log(owner)
       let objToPut = {
         ...owner,
-        favorites:owner.favorites[0]?[...owner.favorites, id]:[id]
+        favorites: owner.favorites[0] ? [...owner.favorites, id] : [id]
       }
-      setFavorites([...favorites,id])
+      setFavorites([...favorites, id])
       console.log(objToPut)
       await axios.put("http://localhost:3001/owners/addFavorite", objToPut)
     }
-    else{
+    else {
       const AllOwners = await axios.get("http://localhost:3001/owners")
       // console.log(owner)
-      const owner = AllOwners.data.find(x=>x.email === user.email)
+      const owner = AllOwners.data.find(x => x.email === user.email)
       console.log(owner)
       let objToPut = {
         ...owner,
-        favorites:owner.favorites[0]?owner.favorites.filter(x=>x !== id):[]
+        favorites: owner.favorites[0] ? owner.favorites.filter(x => x !== id) : []
       }
-      setFavorites(favorites.filter(x=>x !== id))
+      setFavorites(favorites.filter(x => x !== id))
       console.log(objToPut)
       await axios.put("http://localhost:3001/owners/addFavorite", objToPut)
     }
-    
+
   }
 
   return (
-    // <Link to={`/shop/${id}`}>
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <img src={profilePicture} alt="" className={styles.cardImg} />
-        <img src={!isFavorite?"https://static.vecteezy.com/system/resources/previews/005/218/113/non_2x/heart-contour-outline-line-icon-black-color-illustration-image-thin-flat-style-vector.jpg":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/640px-Heart_coraz%C3%B3n.svg.png"} alt="" className={styles.addFav} onClick={addFavorite}/>
-        <div className={styles.cardInfo}>
-          <h2 className={styles.cardTitle}>{name}</h2>
-          <div className={styles.cardBottom}>
-            <p className={styles.price}>${price}</p>
-            {/* <button className={styles.addButton} onClick={()=>{
+    <Link to={`/shop/${id}`}>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <img src={profilePicture} alt="" className={styles.cardImg} />
+          <img src={!isFavorite ? "https://static.vecteezy.com/system/resources/previews/005/218/113/non_2x/heart-contour-outline-line-icon-black-color-illustration-image-thin-flat-style-vector.jpg" : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/640px-Heart_coraz%C3%B3n.svg.png"} alt="" className={styles.addFav} onClick={addFavorite} />
+          <div className={styles.cardInfo}>
+            <h2 className={styles.cardTitle}>{name}</h2>
+            <div className={styles.cardBottom}>
+              <p className={styles.price}>${price}</p>
+              {/* <button className={styles.addButton} onClick={()=>{
               dispatch({
               type:TYPES.ADD_TO_CART,
               payload:id,
             })}}>Agregar al carrito</button> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    // </Link>
+    </Link>
   );
 };
 
