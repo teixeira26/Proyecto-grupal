12e27346f5905ff64f69a4f4638645f00f916e6e
shift <teixeira.matheus2626@gmail.com>
2022-06-04 21:26:16 -0300

    agregado renderizado de numero en navbar favoritos

--- client/src/redux/actions-type/petshopActionsTypes.js
+++ client/src/redux/actions-type/petshopActionsTypes.js
@@ -9,4 +9,5 @@ export const CHARGE_CART = 'CHARGE_CART';
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const CLEAR_CART = "CLEAR_CART";
 export const ADD_ITEM = "ADD_ITEM"
-export const DELETE_ITEM = "DELETE_ITEM"
+export const DELETE_ITEM = "DELETE_ITEM";
+export const ADD_FAVORITE_REDUX = 'ADD_FAVORITE_REDUX'
