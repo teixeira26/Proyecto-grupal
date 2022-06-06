2b5801263f34abf64a0db12e4cb0a612d6eb4bb6
Alan Topczylo <alantopczylo@gmail.com>
2022-06-05 21:13:27 -0300

    Checkout

--- client/src/Components/ShoppingCart/ShoppingCart.jsx
+++ client/src/Components/ShoppingCart/ShoppingCart.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import CartItem from "./CartItem";
 import "../../index.css";
 import NavBarShop from "../NavBar/NavBarShop";
+import Footer from "../Footer/Footer";
 import { useDispatch, useSelector } from "react-redux";
 import { chargeCart, clearAllCart } from "../../redux/actions/petshopActions";
-import { useAuth0 } from '@auth0/auth0-react';
-import { Link, NavLink } from 'react-router-dom';
-import MercadoPago from '../Shop/MercadoPago/MercadoPago';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link, NavLink } from "react-router-dom";
+import MercadoPago from "../Shop/MercadoPago/MercadoPago";
+import styles from "../ShoppingCart/ShoppingCart.module.css";
+import InContainer from "../GlobalCss/InContainer.module.css";
 
 const ShoppingCart = () => {
   const dispatch = useDispatch();
@@ -41,34 +44,84 @@ const ShoppingCart = () => {
 
   return (
     <div>
-              <NavBarShop />
-      <h2>Carrito de Compras</h2>
+      <NavBarShop />
+      <div className={InContainer.container}>
+        <NavLink to="/shop">
+          <img src="./assets/img/arrow-left.svg" alt="" />
+        </NavLink>
 
-      <NavLink to="/shop">
-        <p>Volver al shop</p>
-      </NavLink>
+        <h2 className={styles.cartTitle}>Carrito</h2>
 
+        <h3 className={styles.cartProducts}>Productos a comprar</h3>
 
-            <h3>Productos</h3>
-            <article className="box">
-                <button onClick={clearCart}>Limpiar Carrito</button>
-            </article>
-            <article className="box grid-responsive">
-                {
-                    cart && cart.length > 0 ? cart.map((item, index) => (   
-                        <CartItem key={index} name={item.name} image={item.profilePicture} price={item.price} quantity={item.quantity} id={item.id} stock={item.stock} />//delFromCart={delFromCart}
-                    )) : <h1>No hay ningún producto en el carrito</h1>
-                }
-                <h3>total: {total}</h3>
-                {/* <MercadoPago cart={cart}
-                              />  */}
-                <Link to='/purchaseConfirmation'>
-                <button>Continuar al pago</button>
+        <div className={styles.cartSubtitles}>
+          <table className={styles.table}>
+            <thead>
+              <tr className={styles.tr}>
+                <th className={styles.th}>
+                  <p>Imagen</p>
+                </th>
+                <th className={styles.th}>
+                  <p className={styles.product}>Producto</p>
+                </th>
+                <th className={styles.th}>
+                  <p className={styles.price}>Precio</p>
+                </th>
+                <th className={styles.th}>
+                  <p className={styles.price}>Cantidad</p>
+                </th>
+                <th className={styles.th}>
+                  <p className={styles.price}>Subtotal</p>
+                </th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <article className={styles.productList}>
+          {cart && cart.length > 0 ? (
+            cart.map((item, index) => (
+              <CartItem
+                key={index}
+                name={item.name}
+                image={item.profilePicture}
+                price={item.price}
+                quantity={item.quantity}
+                id={item.id}
+                stock={item.stock}
+              />
+            ))
+          ) : (
+            <p className={styles.noProducts}>
+              No hay ningún producto en el carrito
+            </p>
+          )}
+        </article>
+
+        <div className={styles.cartBottom}>
+          <div>
+            <div className={styles.totalFlex}>
+              <h3>Total: ${total}</h3>
+            </div>
+
+            <div className={styles.purchase}>
+              {cart && cart.length > 0 ? (
+                <Link to="/purchaseConfirmation">
+                  <button>Continuar al pago</button>
                 </Link>
-                </article>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          </div>
 
-
-      
+          {/* <article>
+            <button onClick={clearCart}>Limpiar Carrito</button>
+          </article> */}
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
     </div>
   );
 };
