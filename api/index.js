a09358b72057a940985d2515828e816087defe8b
shift <teixeira.matheus2626@gmail.com>
2022-06-09 21:32:25 -0300

    rutas de evento finales

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const {server, app} = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 conn.sync({ force: false}).then(() => {
-  
+
   app.listen(3001, () => {
 
     console.log('%s listening at 3001');
